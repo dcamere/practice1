505ed2573bc929fe1c3721894de8213d6cea981d
Smogg PC <spazzaglia98@gmail.com>
2022-10-22 23:00:36 -0500

    Added min and max functionality

--- script.js
+++ script.js
@@ -2,28 +2,42 @@ let count;
 let minusBtn;
 let plusBtn;
 let inputText;
+let min;
+let max;
 
 // Reasign variable values
 const cache = () => {
     minusBtn = document.querySelector(".spinner__minus");
     plusBtn = document.querySelector(".spinner__plus");
     inputText = document.querySelector(".spinner__text");
-    count = inputText.value;
+    count = parseInt(inputText.value);
+    min = parseInt(inputText.dataset.min);
+    max = parseInt(inputText.dataset.max);
+
 };
 
 // Code that we want to execute at first
 const setup = () => {
+  console.log(typeof count)
      
 };
 
 // Create all custom functions
 const handleCount = (operation) => {
   if (operation === "plus") {
+    if(count === max){
+      console.log(`You can't go above ${max}`);
+      return;
+    }
     count++;
   } else {
+    if(count <= min){
+      console.log(`You can't go below ${min}`);
+      return;
+    }
     count--;
   }
-
+  
   inputText.value = count;
   console.log(count);
   // operation === "plus" ? count++ : count--;
