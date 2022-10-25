e78f82e63e844b2a1a21a8098ca6d35efcf9ecea
Salvatore Pazzaglia <smogg21@DESKTOP-KN47O2R.localdomain>
2022-10-24 21:13:10 -0500

    Added button functionality and fetch

--- dadjokes.js
+++ dadjokes.js
@@ -1,21 +1,27 @@
 let btnSearch
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const API_ENDPOINT = "https://dad-jokes.p.rapidapi.com/random/joke";
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e02e435b79mshb9262274bda6b69p175543jsn8e52920af1b0',
+		'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+	}
+};
 
 const cache = () =>{
     btnSearch = document.querySelector(".btnSearch");
+    contenido = document.querySelector(".contenido")
+    punchline = document.querySelector(".punchline")
     
 };
 const setup = () =>{
 };
 
 const getJoke = () =>{
-    fetch (API_ENDPOINT)
+    fetch (API_ENDPOINT, options)
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
-   
-    
+    .then(response => 
+        contenido.innerHTML = `${response.body[0].setup}` + "<br />" + `${response.body[0].punchline}`)
 }
 
 const bind = () =>{
